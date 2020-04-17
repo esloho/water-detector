373d725dd25442127a58a2cb1bcd5d4cc1b4cf52
Esther <esloho@gmail.com>
2020-04-17 19:42:35 +0200

    Add a new node to the sorted list

---
+++ lib/score_sorting.rb
@@ -0,0 +1,28 @@
+
+module ScoreSorting
+  extend self
+
+  def add_score(x, y, score, top_score)
+    node = ScoreNode.new(x, y, score)
+
+    return node if top_score.nil?
+
+    previous_node = find_previous_node(node, top_score)
+
+    if previous_node.nil?
+      node.add_before(top_score)
+      return node
+    end
+
+    node.add_after(previous_node)
+
+    top_score
+  end
+
+  def find_previous_node(node, current)
+    return current if current.next_node.nil?
+    return current.prev_node if node.score > current.score
+
+    find_previous_node(node, current.next_node)
+  end
+end
---
+++ spec/score_sorting_spec.rb
@@ -0,0 +1,117 @@
+require 'score_sorting'
+
+describe  'ScoreSorting' do
+  include ScoreSorting
+
+  describe '#find_previous_top' do
+
+    before(:each) do
+      @top_score = ScoreNode.new(0, 0, 12)
+      @node2 = ScoreNode.new(1, 3, 9, @top_score)
+      @top_score.next_node = @node2
+      @node3 = ScoreNode.new(2, 0, 7, @node2)
+      @node2.next_node = @node3
+    end
+
+    it 'should return nil when new node has the higher score' do
+      # Given
+      node = ScoreNode.new(0, 1, 19)
+
+      # When
+      previous_node = ScoreSorting.find_previous_node(node, @top_score)
+
+      # Then
+      expect(previous_node).to be_nil
+    end
+
+    it 'should return last node when new node has the lowest score' do
+      # Given
+      node = ScoreNode.new(0, 1, 5)
+
+      # When
+      previous_top = ScoreSorting.find_previous_node(node, @top_score)
+
+      # Then
+      expect(previous_top).to be == @node3
+    end
+
+    it 'should return root node when new node has the second highest score' do
+      # Given
+      node = ScoreNode.new(0, 1, 10)
+
+      # When
+      previous_top = ScoreSorting.find_previous_node(node, @top_score)
+
+      # Then
+      expect(previous_top).to be == @top_score
+    end
+
+  end
+
+  describe '#add_score' do
+
+    before(:each) do
+      @top_score = ScoreNode.new(0, 0, 12)
+      @node2 = ScoreNode.new(1, 3, 9, @top_score)
+      @top_score.next_node = @node2
+      @node3 = ScoreNode.new(2, 0, 7, @node2)
+      @node2.next_node = @node3
+    end
+
+    it 'should add a new node as root if highest score' do
+      # Given
+      x = 0
+      y = 1
+      score = 19
+
+      # When
+      root = ScoreSorting.add_score(x, y, score, @top_score)
+
+      # Then
+      expect(root.score).to be == score
+      expect(root.coord_x).to be == x
+      expect(root.coord_y).to be == y
+      expect(root.prev_node).to be_nil
+      expect(root.next_node).to be == @top_score
+      expect(@top_score.prev_node).to be == root
+    end
+
+    it 'should add a new node at the end if lowest score' do
+      # Given
+      x = 0
+      y = 1
+      score = 5
+
+      # When
+      root = ScoreSorting.add_score(x, y, score, @top_score)
+
+      # Then
+      new_node = @node3.next_node
+      expect(root).to be == @top_score
+      expect(new_node.coord_x).to be == x
+      expect(new_node.coord_y).to be == y
+      expect(new_node.score).to be == score
+      expect(new_node.prev_node).to be == @node3
+      expect(new_node.next_node).to be_nil
+    end
+
+    it 'should add a new node after root if second highest score' do
+      # Given
+      x = 0
+      y = 1
+      score = 10
+
+      # When
+      root = ScoreSorting.add_score(x, y, score, @top_score)
+
+      # Then
+      new_node = root.next_node
+      expect(root).to be == @top_score
+      expect(new_node.coord_x).to be == x
+      expect(new_node.coord_y).to be == y
+      expect(new_node.score).to be == score
+      expect(new_node.prev_node).to be == @top_score
+      expect(new_node.next_node).to be == @node2
+    end
+  end
+end 
