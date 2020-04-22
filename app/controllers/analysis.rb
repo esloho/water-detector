41cf8bcff8d34a6a8b245aad563f2b0bed071287
Esther <esloho@gmail.com>
2020-04-22 19:47:25 +0200

    Show errors in form page

--- app/controllers/analysis.rb
+++ app/controllers/analysis.rb
@@ -7,11 +7,17 @@ WaterDetector::App.controllers :analysis do
   end
 
   post :index do
-    t = params[:t] || 0
-    @number_results, @size, grid = InputData.parse(t, params[:n], params[:grid])
+    begin
+      t = params[:t] || 0
+      @number_results, @size, grid = InputData.parse(t, params[:n], params[:grid])
 
-    @data = LocationSelection.get_heatmap_data(@size, grid)
+      @data = LocationSelection.get_heatmap_data(@size, grid)
 
-    render 'analysis/results'
+      render 'analysis/results'
+
+    rescue Exception => e
+      @error = e
+      render 'analysis/index'
+    end
   end
 end
