f11325437719c466b848824bb5178b583de9fed3
Esther <esloho@gmail.com>
2020-04-22 12:58:59 +0200

    Generate scores grid for heatmap

--- lib/location_selection.rb
+++ lib/location_selection.rb
@@ -5,22 +5,32 @@ module LocationSelection
   extend self
 
   def find_top_locations(t, n, grid)
-    sorted_scores = get_sorted_area_scores(n, grid)
+    scores_grid, sorted_scores = get_sorted_area_scores(n, grid)
     print_top_locations(t, sorted_scores)
+
+    scores_grid
+  end
+
+  def get_heatmap_data(n, grid)
+    scores_grid, top_location = get_sorted_area_scores(n, grid)
+
+    [scores_grid, top_location.score]
   end
 
   def get_sorted_area_scores(n, grid)
     last_pos = n - 1
     sorted_locations = nil
+    scores_grid = []
 
     0.upto(last_pos) do |x|
       0.upto(last_pos) do |y|
         score = ScoreCalculation.get_area_score(x, y, n, grid)
+        scores_grid.push([x, y, score])
         sorted_locations = LocationSorting.add_location(x, y, score, sorted_locations)
       end
     end
 
-    sorted_locations
+    [scores_grid, sorted_locations]
   end
 
   def print_top_locations(t, location)
