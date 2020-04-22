2b378e9659e9af622c520f8ed3c423c579d1e2fe
Esther <esloho@gmail.com>
2020-04-22 17:47:32 +0200

    Add form and use real data for analysis

--- app/controllers/analysis.rb
+++ app/controllers/analysis.rb
@@ -3,8 +3,11 @@ require_relative '../../lib/location_selection'
 WaterDetector::App.controllers :analysis do
 
   get :index do
-    @size = 4
-    grid = ([2, 3, 2, 1, 4, 4, 2, 0, 3, 4, 1, 1, 2, 3, 4, 4]).to_a
+    render 'analysis'
+  end
+
+  post :index do
+    @number_results, @size, grid = InputData.transform(params[:t], params[:n], params[:grid])
 
     @data = LocationSelection.get_heatmap_data(@size, grid)
 
